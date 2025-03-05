ff060d88b2be1cff723538431115b458614d9afb
Ggasyd <gagnon.gasyd@gmail.com>
2025-03-05 15:12:46 +0100

    api mod

--- IFA/app/main.py
+++ IFA/app/main.py
@@ -43,6 +43,6 @@ async def root():
 
 
 # Lancer l'application uniquement si exécutée directement
-if __name__ == "__main__":
+"""if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)"""
